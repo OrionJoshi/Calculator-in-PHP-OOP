86d31f01c6ace55213d275e4eb7c42e48e6668d7
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-04 09:45:14 +0545

    Calculator html form add

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="includes/calc.inc.php" method="post">
+        <p> My Own Calculator </p>
+        <input type="number" name="num1" placeholder="First Number">
+        <select name="oper">
+            <option value="add">Addition</option>
+            <option value="sub">Subtraction</option>
+            <option value="div">Division</option>
+            <option value="mul">Multipication</option>
+        </select>
+        <input type="number" name="num2" placeholder = "Second number">
+        <button type="submit" name = "submit"> Calculator </button>
+    
+    </form>
+
+</body>
+</html>
