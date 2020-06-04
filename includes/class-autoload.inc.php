f2e0b71970dc4be282da2821ad7f42f4a8c2ce47
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-04 13:15:42 +0545

    Fix issues

--- includes/class-autoload.inc.php
+++ includes/class-autoload.inc.php
@@ -3,12 +3,12 @@
     spl_autoload_register('myAutoLoader');
 
     function myAutoLoader($className) {
-        $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URL'];
+        $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
         if(strpos($url, 'includes') !== false){
             $path = '../classes/';
         }
-        esle{
+        else{
             $path = 'classes/';
         }
         $extension = '.class.php';
